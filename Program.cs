2f684b7f11cb0d8573701e946827346fbd816c01
Fatih Ozdemir <fatih.zdemr@gmail.com>
2022-02-05 05:05:02 +0300

    first commit

---
+++ Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace while_foreach
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Console.Write("Bir sayı giriniz: ");
+            // int num = int.Parse(Console.ReadLine());
+            // int counter = 1;
+            // int sum = 0;
+            // while (counter <= num)
+            // {
+            //     sum += counter;
+            //     counter++;
+            // }
+
+            // Console.WriteLine(sum / num);
+
+            // char character = 'a';
+            // while (character <= 'z')
+            // {
+            //     Console.Write(character);
+            //     character++;
+            // }
+            // Console.WriteLine();
+
+            string[] arabalar = {"BMW", "Ford", "Toyota", "Nissan"};
+            foreach (var araba in arabalar)
+            {
+                Console.WriteLine(araba);
+            }
+        }
+    }
+}
